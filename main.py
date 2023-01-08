13a47d72e8ba407f821f6c6b1740b9b79fdae1b7
unisebcin <97784790+unisebcin@users.noreply.github.com>
2023-01-08 21:36:51 +0300

    Initial setup

---
+++ main.py
@@ -0,0 +1,52 @@
+from dotenv import load_dotenv
+import os
+import logging
+from logging import config
+load_dotenv('.env')
+logging.config.fileConfig(fname='log.conf', disable_existing_loggers=False)
+logger = logging.getLogger('Yahoo')
+
+logger.info('Yahoo Mail Started')
+pass_ = os.environ.get('yahoo_app_password')
+print(pass_)
+
+import smtplib
+from email.mime.text import MIMEText
+SMTP_SERVER = "smtp.mail.yahoo.com"
+SMTP_PORT = 587
+SMTP_USERNAME = "sebcin2001@yahoo.com"
+SMTP_PASSWORD = pass_
+EMAIL_FROM = "sebcin2001@yahoo.com"
+EMAIL_TO = "setsebn@gmail.com"
+EMAIL_SUBJECT = "REMINDER:"
+co_msg = """
+Hello sedat! Just wanted to send a friendly appointment
+reminder for your appointment:
+[Company]
+Where: [companyAddress]
+Time: [appointmentTime]
+Company URL: [companyUrl]
+Change appointment?? Add Service??
+change notification preference (text msg/email)
+"""
+def send_email():
+    logger.info('Send Mail Started')
+    msg = MIMEText(co_msg)
+    msg['Subject'] = EMAIL_SUBJECT
+    msg['From'] = EMAIL_FROM
+    msg['To'] = EMAIL_TO
+    debuglevel = True
+    mail = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    mail.set_debuglevel(debuglevel)
+    mail.starttls()
+    try:
+        mail.login(SMTP_USERNAME, SMTP_PASSWORD)
+        mail.sendmail(EMAIL_FROM, EMAIL_TO, msg.as_string())
+    except Exception as e:
+        logger.error('Error : ', e)
+    else:
+        logger.info('Mail sent')
+    mail.quit()
+
+if __name__=='__main__':
+    send_email()
